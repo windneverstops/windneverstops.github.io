e5f7dbb2a10197c2effca4d74f98f28977248d60
danielhong <dan.hong378@gmail.com>
2024-03-07 13:08:34 +1100

    removed actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-    basePath: "/website",
+
   };
 
 export default nextConfig;
