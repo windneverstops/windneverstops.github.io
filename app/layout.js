48197ee12ec90724be5a7f9aab8150361a91f418
suspiciouscap <108613049+suspiciouscap@users.noreply.github.com>
2024-03-10 23:49:02 +1100

    Added main dashboard with links and animations for home page

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="overflow-y-hidden">
+    <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,12 +1,18 @@
 "use client";
+
+import Nav from "@/components/Nav";
 import { Source_Code_Pro } from "next/font/google";
-import { useState, useRef } from "react";
+import { useRef } from "react";
+import MovingShadow from "@/components/MovingShadow";
 
 const source_code_pro = Source_Code_Pro({
   subsets: ["latin"],
 })
 
+
 export default function Home() {
+
+
   const messageRef = useRef(null);
 
   const handleMessageClick = () =>{
@@ -17,20 +23,49 @@ export default function Home() {
   }
 
   return (
-    <main className="overflow-y-scroll h-screen">
-      <div className={`static flex flex-col justify-center items-center text-center h-screen sm:text-5xl text-3xl gap-y-14 p-12 ${source_code_pro.className}`}>
-        <div>
-          G&apos;day, my name is
-          <span className="text-6xl text-red-400 pl-7 sm:text-9xl">Daniel Hong.</span>
-        </div>
-        <div>
-          I&apos;m a penultimate Software Engineering student at a GO8 uni.
+    <main className="h-screen">
+      <div className={`relative flex flex-col lg:items-center items-start h-screen sm:text-5xl text-3xl p-12 ${source_code_pro.className}`}>
+        <Nav/>
+        <div className="grow flex flex-col justify-around h-auto">
+          <div className="flex flex-col gap-y-4 items-center">
+            <div className="text-4xl">
+            G&apos;day, my name is
+            </div>
+            <div>
+            <span className="text-5xl text-red-400 pl-7 sm:text-8xl">Daniel Hong.</span>
+            </div>
+            <div className="pt-10 text-wrap">
+              I&apos;m a penultimate Software Engineering student at a GO8 uni.
+            </div> 
+          </div>
+          <div className="flex flex-col items-center gap-y-12">
+            <div>
+              <a href="https://onedrive.live.com/download?resid=DFBE1DA5AC2963A8%216446&authkey=!APfcjMz0T8FAC1M&em=2">
+                <MovingShadow><svg width="30" height="30" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.50005 1.04999C7.74858 1.04999 7.95005 1.25146 7.95005 1.49999V8.41359L10.1819 6.18179C10.3576 6.00605 10.6425 6.00605 10.8182 6.18179C10.994 6.35753 10.994 6.64245 10.8182 6.81819L7.81825 9.81819C7.64251 9.99392 7.35759 9.99392 7.18185 9.81819L4.18185 6.81819C4.00611 6.64245 4.00611 6.35753 4.18185 6.18179C4.35759 6.00605 4.64251 6.00605 4.81825 6.18179L7.05005 8.41359V1.49999C7.05005 1.25146 7.25152 1.04999 7.50005 1.04999ZM2.5 10C2.77614 10 3 10.2239 3 10.5V12C3 12.5539 3.44565 13 3.99635 13H11.0012C11.5529 13 12 12.5528 12 12V10.5C12 10.2239 12.2239 10 12.5 10C12.7761 10 13 10.2239 13 10.5V12C13 13.1041 12.1062 14 11.0012 14H3.99635C2.89019 14 2 13.103 2 12V10.5C2 10.2239 2.22386 10 2.5 10Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
+                  <div className="p-2">
+                    Download CV
+                  </div>
+                </MovingShadow>
+              </a>
+            </div>
+            <div className=" w-full flex justify-center animate-bounce">
+              <div className="flex flex-col">
+                <svg  width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
+                <svg  width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
+                <svg  width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
+                <svg  width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path></svg>
+              </div>
+            </div>
+          </div>
+          
         </div>
       </div>
       <div className="static h-screen">
-        sdfsdf
+        Content to be filled
       </div>
-      <div ref={messageRef} id="t" className="absolute text-center bottom-0 inset-x-0 h-16 bg-gray-200 flex items-center justify-center">
+
+
+      <div ref={messageRef} id="t" className="fixed text-center bottom-0 inset-x-0 h-16 bg-gray-200 flex items-center justify-center">
         This website is currently under construction! Check back soon.
         <button className="absolute top-2 right-0 p-4 bg-gray-200" onClick={handleMessageClick}>
           <svg 
@@ -39,7 +74,7 @@ export default function Home() {
             viewBox="0 0 15 15" 
             fill="none" 
             xmlns="http://www.w3.org/2000/svg">
-              <path d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
+              <path d="M12.8536 2.85355C13.0488 2.65829 13.0488 2.34171 12.8536 2.14645C12.6583 1.95118 12.3417 1.95118 12.1464 2.14645L7.5 6.79289L2.85355 2.14645C2.65829 1.95118 2.34171 1.95118 2.14645 2.14645C1.95118 2.34171 1.95118 2.65829 2.14645 2.85355L6.79289 7.5L2.14645 12.1464C1.95118 12.3417 1.95118 12.6583 2.14645 12.8536C2.34171 13.0488 2.65829 13.0488 2.85355 12.8536L7.5 8.20711L12.1464 12.8536C12.3417 13.0488 12.6583 13.0488 12.8536 12.8536C13.0488 12.6583 13.0488 12.3417 12.8536 12.1464L8.20711 7.5L12.8536 2.85355Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd"></path>
             </svg>
           </button>
       </div>
